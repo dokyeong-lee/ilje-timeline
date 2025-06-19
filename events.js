224f7e188ebbedf5609258e6eea62dd57180dd78
dakyoung_lee <leedk5201@gmail.com>
2025-06-19 14:57:43 +0900

    Update events.js

--- events.js
+++ events.js
@@ -1,440 +1,51 @@
 const events = {
-  "일제의 국권 침탈, 조선 총독부 설치": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "덕수궁 석조전 완공": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "회사령 공포": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "토지 조사 사업(~1918년)": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "홍범도 등 연해주 의병 국내 진격 작전": {
-    "description": "",
-    "activists": [
-      "홍범도"
-    ],
-    "link": ""
-  },
-  "조선 총독부, 토지 조사령 공포": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "조선 태형령 시행": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "임병찬, 독립 의군부 조직": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "박용만, 하와이에서 대조선 국민 군단 조직": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "이상설, 대한 광복군 정부 수립": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "호남선·경원선 철도 개통": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "대한 광복회 조직": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "이동휘 등, 한인사회당 결성(사회주의 정당)": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "여운형 등, 신한청년당 결성(항일 독립운동 단체)": {
-    "description": "",
-    "activists": [
-      "여운형"
-    ],
-    "link": ""
-  },
-  "2·8 독립 선언": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
   "3·1 운동": {
-    "description": "",
-    "activists": [],
-    "link": ""
+    "description": "1919년 3월 1일 전국에서 일어난 대규모 비폭력 독립운동. 민족 대표 33인이 독립선언서를 발표하고, 전국 각지에서 만세 시위가 벌어졌습니다.",
+    "activists": [
+      "유관순",
+      "손병희",
+      "이승훈"
+    ],
+    "link": "https://contents.history.go.kr/front/hm/view.do?treeId=010102&tabId=01&levelId=hm_114_0040_0020_0030"
   },
   "대한민국 임시 정부 수립(상하이)": {
-    "description": "",
-    "activists": [],
-    "link": ""
+    "description": "3·1 운동 이후 독립운동가들이 상하이에 세운 임시정부로, 외교활동과 무장투쟁을 주도했습니다.",
+    "activists": [
+      "이승만",
+      "김구",
+      "안창호"
+    ],
+    "link": "https://contents.history.go.kr/front/hm/view.do?treeId=010102&tabId=01&levelId=hm_114_0040_0020_0040"
   },
   "의열단 조직": {
-    "description": "",
-    "activists": [],
-    "link": ""
+    "description": "1919년 김원봉 주도로 결성된 무장 독립운동 단체로, 일제 주요 시설과 요인을 공격하는 의열 투쟁을 전개했습니다.",
+    "activists": [
+      "김원봉",
+      "나석주",
+      "윤봉길"
+    ],
+    "link": "https://contents.history.go.kr/front/hm/view.do?treeId=010102&tabId=01&levelId=hm_114_0040_0020_0070"
   },
-  "한국인이 처음으로 영화 제작": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "〈조선일보〉·〈동아일보〉 창간": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "조선교육회 설립": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "6월, 봉오동 전투(홍범도)": {
-    "description": "",
+  "봉오동 전투(홍범도)": {
+    "description": "1920년 6월, 홍범도 부대가 일본군을 상대로 대승을 거둔 전투입니다.",
     "activists": [
       "홍범도"
     ],
-    "link": ""
+    "link": "https://contents.history.go.kr/front/hm/view.do?treeId=010102&tabId=01&levelId=hm_114_0040_0030_0010"
   },
-  "10월, 청산리 대첩(김좌진, 홍범도)": {
-    "description": "",
+  "청산리 대첩(김좌진, 홍범도)": {
+    "description": "1920년 10월 김좌진, 홍범도 부대가 청산리에서 일본군과 치열한 전투 끝에 대승을 거둔 독립 전쟁입니다.",
     "activists": [
-      "홍범도",
-      "김좌진"
+      "김좌진",
+      "홍범도"
     ],
-    "link": ""
-  },
-  "10월, 간도 참변 발생": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "조만식, 조선 물산 장려회 조직": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "자유시 참변": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "조선어 연구회 설립": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "이광수, 〈민족 개조론〉 발표": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "어린이날 제정": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "암태도 소작 쟁의( ~1924년)": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "민립 대학 설립 운동": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "조선 물산 장려회 창립(물산 장려 운동 활발히 전개)": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "참의부 조직": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "정의부 조직": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "경성 제국 대학 설립": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "신민부 조직": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "조선 공산당 결성": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "6·10 만세 운동": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "나석주, 동양 척식 주식ᕴ와 조선 식산 은에 폭탄 투척": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "신간회 창립": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "근우회 창립": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "라디오 방송 시작": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "원산 총파업( ~1929)": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "광주 학생 항일 운동": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "일제의 만주 침략(만주사변)": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "신간회 해소": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "조선어 연구회, 조선어 학회로 개칭": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "김구, 한인 애국단 조직": {
-    "description": "",
-    "activists": [
-      "김구"
-    ],
-    "link": ""
-  },
-  "〈동아일보〉, 브나로드 운동 전개( ~1934)": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "이봉창, 일왕 폭살 시도": {
-    "description": "",
-    "activists": [
-      "이봉창"
-    ],
-    "link": ""
+    "link": "https://contents.history.go.kr/front/hm/view.do?treeId=010102&tabId=01&levelId=hm_114_0040_0030_0020"
   },
   "윤봉길, 홍커우 공원 의거": {
-    "description": "",
+    "description": "1932년 상하이 홍커우 공원에서 윤봉길이 일본군 수뇌부를 향해 폭탄을 던져 전 세계에 한국 독립의지를 알린 사건입니다.",
     "activists": [
       "윤봉길"
     ],
-    "link": ""
-  },
-  "조선 혁명과 한국 독립이 한·중 연합군 조직": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "한글 맞춤법 통일안 제정": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "조선 총독부, 농촌 진흥 운동 시작": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "조선 농지령 공포": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "진단 학회 조직": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "안재홍 등, 조선학 운동 전개": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "민족 혁명당 결성": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "한국 국민당 결성": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "손기정, 베를린 올림픽 대회 마라톤 우승. 〈동아일보〉 일장기 말살 사건": {
-    "description": "",
-    "activists": [
-      "손기정"
-    ],
-    "link": ""
-  },
-  "연해주 동포, 중앙아시아로 강제 이주": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "중일 전쟁 시작. 황국 신민의 서사 제정": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "신사 참배 강요. 화신 백화점 개점": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "한글 교육 금지": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "국가 총동원법 공포": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "김원봉, 조선의용대 조직": {
-    "description": "",
-    "activists": [
-      "김원봉"
-    ],
-    "link": ""
-  },
-  "국민 징용령 공포. 1945년까지 45만 명 연행": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "일제식 성명 강요 실시 (창씨개명)": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "한국 독립당 창당": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "임시정부, 한국광복군 창설": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "〈조선일보〉·〈동아일보〉 폐간": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "대한민국 임시 정부, 건국 강령 발표 및 대일 선전 포고": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "조선의용대, 한국광복군에 편입": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "조선어 학회 사건": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "서울 인구 100만 명을 넘어섬": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "일제, 징병제·학도 지원병제 실시로 조선 청년을 일본군으로 끌고 감": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "조선 총독부, 여자 정신대 근무령 공포 및 시행": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "여운형, 건국 동맹 결성": {
-    "description": "",
-    "activists": [
-      "여운형"
-    ],
-    "link": ""
-  },
-  "8·15 해방": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "여운형, 조선 건국 준비 위원회 발족": {
-    "description": "",
-    "activists": [
-      "여운형"
-    ],
-    "link": ""
-  },
-  "미소 군정 실시": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "모스크바 3국 외상 회의 개최": {
-    "description": "",
-    "activists": [],
-    "link": ""
-  },
-  "신탁 통치 반대 운동 일어남": {
-    "description": "",
-    "activists": [],
-    "link": ""
+    "link": "https://contents.history.go.kr/front/hm/view.do?treeId=010102&tabId=01&levelId=hm_114_0040_0070_0020"
   }
 };
